796aa47959136a491d445949d3c001c154c1fae7
Clara <clara.grillet@sciencespo.fr>
2018-10-13 13:39:20 +0200

    edit allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0ui4-7h$!+&)y%*fweqmuuc52$*k36%2&+-y*a1$$e%s0f20*3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
