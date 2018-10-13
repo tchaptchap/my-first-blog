af787e8a223cc773a4840fd4f275e78be3201132
Clara <clara.grillet@sciencespo.fr>
2018-10-13 16:40:25 +0200

    python manage.py collectstatic

--- blog/views.py
+++ blog/views.py
@@ -32,7 +32,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-    def post_edit(request, pk):
+def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
