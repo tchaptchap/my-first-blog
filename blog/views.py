39dfbd0752c06134af5e9d380263563d13691a6f
Clara <clara.grillet@sciencespo.fr>
2018-10-13 16:38:46 +0200

    Repare views

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail'), pk=post.pk)
+            return redirect('post_detail',pk=post.pk)
     else:
         form = PostForm()
 
